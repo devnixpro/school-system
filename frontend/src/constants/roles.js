ce02f7b9f7b8a5d6443f944acf743c769acfb0da
SBMirza <sbgmirza2@gmail.com>
2026-09-24 13:01:47 +0500

    Saleha baig create next.js setup

--- frontend/src/constants/roles.js
+++
@@ -1,8 +0,0 @@
-﻿export const ROLES = Object.freeze({
-  SUPER_ADMIN: "superadmin",
-  ADMIN: "admin",
-  TEACHER: "teacher",
-  STUDENT: "student",
-  PARENT: "parent",
-});
-export const PRIVILEGED_ROLES = [ROLES.ADMIN, ROLES.SUPER_ADMIN];
